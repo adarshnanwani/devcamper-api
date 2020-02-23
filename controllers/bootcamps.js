81edbae1dcb553da4567efdcddbc93bd47477436
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-02-23 20:39:41 +0530

    custom ErrorHandler class.

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,4 +1,5 @@
 const Bootcamp = require('../models/Bootcamp');
+const ErrorResponse = require('../utils/errorResponse');
 
 //@desc     Get all bootcamps
 //@route    GET /api/v1/bootcamps
@@ -24,13 +25,16 @@ exports.getBootcamp = async (req, res, next) => {
   try {
     const bootcamp = await Bootcamp.findById(req.params.id);
     if (!bootcamp) {
-      res.status(400).json({ success: false });
+      next(
+        new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+      );
     } else {
       res.status(200).json({ success: true, data: bootcamp });
     }
   } catch (err) {
-    //res.status(400).json({ success: false });
-    next(err);
+    next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
   }
 };
 

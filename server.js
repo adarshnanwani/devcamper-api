774307e59b7451e2b3e76ba1e34ed15678e53640
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-02-23 11:52:41 +0530

    Add db connection methods and colors for console.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colora = require('colors');
 const connectDB = require('./config/db');
 
 // Load env vars
@@ -25,13 +26,15 @@ app.use('/api/v1/bootcamps', bootcamps);
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => {
-  console.log(`App listening in ${process.env.NODE_ENV} mode on port ${PORT}!`);
+  console.log(
+    `App listening in ${process.env.NODE_ENV} mode on port ${PORT}!`.yellow.bold
+  );
 });
 
 // Handle unhandled promise rejections - such as wrong DB password
 
 process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`);
+  console.log(`Error: ${err.message}`.red);
   //Close server and exit process
   server.close(() => process.exit(1));
 });

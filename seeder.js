8f08afbf0761ea62d753f26043549c98304342fe
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-03-11 16:38:27 +0530

    Update database seeder for reviews.

--- seeder.js
+++ seeder.js
@@ -12,6 +12,7 @@ dotenv.config({
 const Bootcamp = require('./models/Bootcamp');
 const Course = require('./models/Course');
 const User = require('./models/User');
+const Review = require('./models/Review');
 
 // Connect to database
 mongoose.connect(process.env.MONGO_URI, {
@@ -33,6 +34,10 @@ const users = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/users.json`),
   'utf-8'
 );
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/reviews.json`),
+  'utf-8'
+);
 
 // Import into DB
 const importData = async () => {
@@ -40,6 +45,7 @@ const importData = async () => {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
     await User.create(users);
+    await Review.create(reviews);
     console.log('Data imported...'.green.inverse);
     process.exit();
   } catch (err) {
@@ -53,6 +59,7 @@ const deleteData = async () => {
     await Bootcamp.deleteMany(); // This deletes everything in the collection
     await Course.deleteMany();
     await User.deleteMany();
+    await Review.deleteMany();
     console.log('Data destroyed...'.red.inverse);
     process.exit();
   } catch (err) {

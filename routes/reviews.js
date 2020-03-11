fb48b155281f2fe2bf44bc3c3441908f27f1641f
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-03-11 14:23:21 +0530

    Add get reviews route, and controller.

---
+++ routes/reviews.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router({ mergeParams: true });
+const advancedResults = require('../middleware/advancedResults');
+const { protect, authorize } = require('../middleware/auth');
+const Review = require('../models/Review');
+const { getReviews } = require('../controllers/reviews');
+
+router.route('/').get(
+  advancedResults(Review, {
+    path: 'bootcamp',
+    select: 'name description'
+  }),
+  getReviews
+);
+
+module.exports = router;

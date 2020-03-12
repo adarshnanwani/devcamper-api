2240ea536a0181d503de418759c171d9e5dd5e9b
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-03-12 11:09:01 +0530

    Add udpate review functionality.

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -64,3 +64,30 @@ exports.addReview = asyncHandler(async (req, res, next) => {
     data: review
   });
 });
+
+//@desc     Update review
+//@route    PUT api/v1/reviews/:id
+//@access   Private
+exports.updateReview = asyncHandler(async (req, res, next) => {
+  let review = await Review.findById(req.params.id);
+  if (!review) {
+    return next(
+      new ErrorResponse(`No review with the id of ${req.params.id}`, 404)
+    );
+  }
+
+  // Make sure review belongs to the user
+  if (review.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(new ErrorResponse(`Not authorized to update review`, 401));
+  }
+
+  review = await Review.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
+
+  return res.status(200).json({
+    success: true,
+    data: review
+  });
+});

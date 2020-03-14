827135e885b10dc4c26e385d6605104e208aebc8
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-03-14 21:33:34 +0530

    Add logout functionality.

--- routes/auth.js
+++ routes/auth.js
@@ -7,23 +7,19 @@ const {
   forgotPassword,
   resetPassword,
   updatePassword,
-  updateDetails
+  updateDetails,
+  logout
 } = require('../controllers/auth');
 
 const { protect } = require('../middleware/auth');
 
 router.route('/register').post(register);
-
 router.route('/login').post(login);
-
+router.route('/logout').get(logout);
 router.route('/me').get(protect, getMe);
-
 router.route('/update').put(protect, updateDetails);
-
 router.route('/forgotpassword').post(forgotPassword);
-
 router.route('/resetpassword/:resetToken').put(resetPassword);
-
 router.route('/changepassword').put(protect, updatePassword);
 
 module.exports = router;
